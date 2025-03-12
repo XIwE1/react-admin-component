ee6a0940a3281a9c9ee99bf66b8e103db2f369c8
xiwei <804393870@qq.com>
2025-03-13 07:53:51 +0800

    feat:优化css效果

--- src/layout/aside/index.jsx
+++ src/layout/aside/index.jsx
@@ -1,10 +1,16 @@
 import "./index.less";
-import { DesktopOutlined } from "@ant-design/icons";
+import { GithubOutlined } from "@ant-design/icons";
 
 const Aside = ({ props, children }) => {
+  const jumpToGitHub = () => window.open("https://github.com/XIwE1");
+
   return (
     <div className="aside">
-      <div className="title"><DesktopOutlined /> 后台系统Demo</div>
+      <div className="title" onClick={jumpToGitHub}>
+        <div className="">
+          <GithubOutlined /> 后台系统Demo v0.0.1
+        </div>
+      </div>
       <div className="list_container">{children}</div>
     </div>
   );
