c006e9cf3163612e47b9423fde6992264929f2c7
XIwE1 <804393870@qq.com>
2025-11-19 13:11:08 +0800

    build:同步资源方式通过volume改为rsync

--- src/layout/aside/index.jsx
+++ src/layout/aside/index.jsx
@@ -8,7 +8,7 @@ const Aside = ({ props, children }) => {
     <div className="aside">
       <div className="title" onClick={jumpToGitHub}>
         <div className="">
-          <GithubOutlined /> Admin v0.3.1
+          <GithubOutlined /> Admin v0.3.2
         </div>
       </div>
       <div className="list_container">{children}</div>
